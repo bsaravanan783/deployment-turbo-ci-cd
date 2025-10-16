c19372ec4180cb6312872fce19c1d572745c40c2
bsaravanan783 <bsaravanan783@gmail.com>
2025-10-16 23:06:17 +0530

    add ws

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -9,10 +9,16 @@ app.get("/",(req , res  ) => {
     return res.send("hi this is the msg from server.");
 })
 
-app.get("/signup", (req , res)=> {
+app.get("/signup", async (req , res)=> {
     const username = req.body.username;
     const password = req.body.password;
-    res.send("user signed up successfully !");
+    const user = await client.user.create({
+        data : {
+            username,
+            password
+        }
+    })
+    res.json( { message : "user signed up successfully !", id: user.id});
 })
 
 app.listen(3000);
